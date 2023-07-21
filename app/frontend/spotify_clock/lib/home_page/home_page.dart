39c22cc300bcee286059bd009ed67e0acdde76e2
Janos <janos823@gmail.com>
2023-07-21 21:02:49 +0000

    add custom tool bar class

--- app/frontend/spotify_clock/lib/home_page/home_page.dart
+++ app/frontend/spotify_clock/lib/home_page/home_page.dart
@@ -5,38 +5,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final toolbarHeight = 0.15 * screenSize.height;
-
     return Scaffold(
       backgroundColor: Color(0xFF9E2B25),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        toolbarHeight: toolbarHeight,
-        title: Padding(
-            padding: EdgeInsets.only(
-                top: 0.03 * toolbarHeight, bottom: 0.0 * toolbarHeight),
-            child: Column(children: [
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Text('Bearbeiten',
-                    style: TextStyle(
-                        fontSize: 0.17 * toolbarHeight,
-                        fontWeight: FontWeight.w100,
-                        color: Color(0xFFE29837))),
-                Icon(Icons.add,
-                    color: Color(0xFFE29837), size: 0.2 * toolbarHeight),
-              ]),
-              SizedBox(height: 0.15 * toolbarHeight),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Wecker',
-                      style: TextStyle(
-                          fontSize: 0.32 * toolbarHeight,
-                          fontWeight: FontWeight.bold)))
-            ])),
-        backgroundColor: Color(0xFF213438),
-        foregroundColor: Color(0xFFFFF8F0),
-      ),
+      appBar: CustomAppBar(),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -47,6 +18,42 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
 
-  AppBar newMethod() => AppBar();
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  static const toolbarHeight = 1.3 * kToolbarHeight;
+
+  @override
+  Size get preferredSize => Size.fromHeight(toolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      toolbarHeight: toolbarHeight,
+      title: Padding(
+          padding: EdgeInsets.only(
+              top: 0.03 * toolbarHeight, bottom: 0.0 * toolbarHeight),
+          child: Column(children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text('Bearbeiten',
+                  style: TextStyle(
+                      fontSize: 0.17 * toolbarHeight,
+                      fontWeight: FontWeight.w100,
+                      color: Color(0xFFE29837))),
+              Icon(Icons.add,
+                  color: Color(0xFFE29837), size: 0.2 * toolbarHeight),
+            ]),
+            SizedBox(height: 0.15 * toolbarHeight),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Wecker',
+                    style: TextStyle(
+                        fontSize: 0.32 * toolbarHeight,
+                        fontWeight: FontWeight.bold)))
+          ])),
+      backgroundColor: Color(0xFF213438),
+      foregroundColor: Color(0xFFFFF8F0),
+    );
+  }
 }
