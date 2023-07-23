464f35f0311f4dc504a56cfcac4e11991bd53c1e
Janos <janos823@gmail.com>
2023-07-23 17:41:35 +0200

    refactor custom appbar 	create components folder 	move to different file in components

--- app/frontend/spotify_clock/lib/modules/home/views/home_view.dart
+++ app/frontend/spotify_clock/lib/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:spotify_clock/modules/home/controllers/home_controller.dart';
+import 'package:spotify_clock/components/mainappbar_component.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -55,44 +56,5 @@ class HomeView extends GetView<HomeController> {
             ),
           ))
         ]));
-  }
-}
-
-class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String title;
-  final List<Widget> navigationChildren;
-  final double toolbarHeight;
-
-  MainAppBar({
-    required this.title,
-    required this.navigationChildren,
-    required this.toolbarHeight,
-  });
-
-  @override
-  Size get preferredSize => Size.fromHeight(toolbarHeight);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading: false,
-      toolbarHeight: toolbarHeight,
-      title: Padding(
-          padding: EdgeInsets.only(
-              top: 0.2 * toolbarHeight, bottom: 0.3 * toolbarHeight),
-          child: Column(children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: navigationChildren),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(title,
-                    style: TextStyle(
-                        fontSize: 0.4 * toolbarHeight,
-                        fontWeight: FontWeight.bold)))
-          ])),
-      backgroundColor: Color(0xFF213438),
-      foregroundColor: Color(0xFFFFF8F0),
-    );
   }
 }
