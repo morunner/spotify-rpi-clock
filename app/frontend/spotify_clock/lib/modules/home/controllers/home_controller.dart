c59d0bc76a34aeaa805f6672ddf7f0be0474c3f1
Janos <janos823@gmail.com>
2023-07-23 17:37:32 +0200

    add clock entry feature to home controller

--- app/frontend/spotify_clock/lib/modules/home/controllers/home_controller.dart
+++ app/frontend/spotify_clock/lib/modules/home/controllers/home_controller.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:spotify_clock/data/model/clock_entry_model.dart';
 
 class HomeController extends GetxController {
+  Rx<List<ClockEntryModel>> clockEntries = Rx<List<ClockEntryModel>>([]);
+  late ClockEntryModel clockEntryModel;
+  var itemCount = 0.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -14,5 +20,17 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  addClockEntry(TimeOfDay time, String songTitle, bool enabled) {
+    clockEntryModel =
+        ClockEntryModel(time: time, songTitle: songTitle, enabled: enabled);
+    clockEntries.value.add(clockEntryModel);
+    itemCount.value = clockEntries.value.length;
+  }
+
+  removeCLockEntry(int index) {
+    clockEntries.value.removeAt(index);
+    itemCount.value = clockEntries.value.length;
   }
 }
