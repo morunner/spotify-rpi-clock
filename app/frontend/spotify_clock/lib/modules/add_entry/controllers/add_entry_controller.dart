3e3bbb687808c08fbbf6ecf3e660c169dd324aca
Janos <janos823@gmail.com>
2023-08-08 19:15:32 +0200

    add individual setter function for each clock entry property

--- app/frontend/spotify_clock/lib/modules/add_entry/controllers/add_entry_controller.dart
+++ app/frontend/spotify_clock/lib/modules/add_entry/controllers/add_entry_controller.dart
@@ -1,6 +1,15 @@
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AddEntryController extends GetxController {
+  Map<String, dynamic> clockEntry = {
+    'wakeup_time': '00:00:00',
+    'title': 'title',
+    'artist': 'artist',
+    'enabled': false,
+  };
+
   @override
   void onInit() {
     super.onInit();
@@ -14,5 +23,25 @@ class AddEntryController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  setWakeUpTime(DateTime time) {
+    clockEntry['wakeup_time'] = DateFormat('HH:mm').format(time).toString();
+  }
+
+  setTitle(String title) {
+    clockEntry['title'] = title;
+  }
+
+  setArtist(String artist) {
+    clockEntry['artist'] = artist;
+  }
+
+  setEnabled(bool enabled) {
+    clockEntry['enabled'] = enabled;
+  }
+
+  addClockEntry() async {
+    await Supabase.instance.client.from('clock_entries').insert(clockEntry);
   }
 }
