4f5ca0548aaf20ed0d190527f4876de55af2a207
Janos <janos823@gmail.com>
2023-08-08 19:16:03 +0200

    remove clock entry from homecontroller 	now in addentrycontroller

--- app/frontend/spotify_clock/lib/modules/home/controllers/home_controller.dart
+++ app/frontend/spotify_clock/lib/modules/home/controllers/home_controller.dart
@@ -1,13 +1,7 @@
 import 'package:get/get.dart';
-import 'package:spotify_clock/data/model/clock_entry_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:intl/intl.dart';
 
 class HomeController extends GetxController {
-  Rx<List<ClockEntryModel>> clockEntries = Rx<List<ClockEntryModel>>([]);
-  late ClockEntryModel clockEntryModel;
-  var itemCount = 0.obs;
-
   @override
   void onInit() {
     super.onInit();
@@ -23,19 +17,7 @@ class HomeController extends GetxController {
     super.onClose();
   }
 
-  addClockEntry(
-      DateTime time, String songTitle, String artist, bool enabled) async {
-    final dateTime = DateFormat('HH:mm').format(time).toString();
-
-    await Supabase.instance.client.from('clock_entries').insert({
-      'wakeup_time': dateTime,
-      'title': songTitle,
-      'artist': artist,
-      'enabled': enabled
-    });
-  }
-
-  removeCLockEntry(int id) async {
+  removeClockEntry(int id) async {
     await Supabase.instance.client
         .from('clock_entries')
         .delete()
