27aca3bddf531b6ec418fb9ae8af974054dd2069
Janos <janos823@gmail.com>
2023-08-10 21:13:37 +0000

    simplify code     remove getx and use plain flutter functionalities

--- app/frontend/spotify_clock/lib/src/backend/clock_entry_manager.dart
+++ app/frontend/spotify_clock/lib/src/backend/clock_entry_manager.dart
@@ -1,29 +1,15 @@
-import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class AddEntryController extends GetxController {
+class ClockEntryManager {
+  ClockEntryManager();
+
   Map<String, dynamic> clockEntry = {
     'wakeup_time': '00:00:00',
     'title': 'title',
     'artist': 'artist',
     'enabled': false,
   };
-
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
 
   setWakeUpTime(DateTime time) {
     clockEntry['wakeup_time'] = DateFormat('HH:mm').format(time).toString();
@@ -43,5 +29,12 @@ class AddEntryController extends GetxController {
 
   addClockEntry() async {
     await Supabase.instance.client.from('clock_entries').insert(clockEntry);
+  }
+
+  removeClockEntry(int id) async {
+    await Supabase.instance.client
+        .from('clock_entries')
+        .delete()
+        .match({'id': id});
   }
 }
