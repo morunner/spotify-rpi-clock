d34f9af5f2f3ca1b4c1773635c13a7572284f143
Janos <janos823@gmail.com>
2023-09-13 15:24:18 +0000

    play specified track on specified device

--- app/backend/supabase/functions/play-on-spotify-connect-device/index.ts
+++ app/backend/supabase/functions/play-on-spotify-connect-device/index.ts
@@ -3,9 +3,8 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import * as base64 from "https://deno.land/std@0.194.0/encoding/base64.ts";
-
-console.log("Hello from Functions!")
 
 const spotifyClientId = Deno.env.get("SPOTIFY_CLIENT_ID") as string;
 const spotifyClientSecret = Deno.env.get("SPOTIFY_CLIENT_SECRET") as string;
@@ -17,8 +16,8 @@ serve(async (req) => {
     device_id: device_id,
     provider_refresh_token: provider_refresh_token,
   }
-
-  fetchSpotifyAccessToken(provider_refresh_token).then((resp) => console.log(resp));
+  const provider_access_token = await fetchSpotifyAccessToken(provider_refresh_token);
+  await playOnSpotify(track_id, device_id, provider_access_token);
 
   return new Response(
     JSON.stringify(data),
@@ -43,11 +42,40 @@ async function fetchSpotifyAccessToken(provider_refresh_token: string) {
     body: form,
   });
 
-  return await resp.json();
+  const data = await resp.json();
+  return data['access_token'];
 }
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+async function getContextUriOffsetFromTrackId(track_id: string, provider_access_token: string) {
+  const url = `https://api.spotify.com/v1/tracks/${track_id}`;
+  const headers = {
+    'Content-Type': 'application/json',
+    "Authorization": 'Bearer ' + provider_access_token,
+  };
+  const resp = await fetch(url, {
+    headers: headers,
+  });
+  const data = await resp.json();
+  
+  return { context_uri: data['album']['uri'], offset: data['track_number']-1 };
+}
+
+async function playOnSpotify(track_id: string, device_id: string, provider_access_token: string) {
+  const url = 'https://api.spotify.com/v1/me/player/play';
+  const headers = {
+    'Content-Type': 'application/json',
+    "Authorization": 'Bearer ' + provider_access_token,
+  };
+  const { context_uri, offset } = await getContextUriOffsetFromTrackId(track_id, provider_access_token);
+  const position_ms = 0;
+  const body = {
+    context_uri: context_uri,
+    offset: { position: offset },
+    position_ms: position_ms.toString(),
+  };
+  const resp = await fetch(url + `/?device_id=${device_id}`, {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify(body),
+  });
+}
